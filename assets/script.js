c78660914fe94b7bf4202b4768a5473eadd12a06
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-30 02:35:28 +0100

    Refactor

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,21 @@
 const pageSwap = (newPage, oldPage) => {
   // Changing nav
-  $(`.${newPage}`).attr('class', `${newPage} nav-link current`);
-  $(`.${oldPage}`).attr('class', `${oldPage} nav-link`);
+  $(`.nav-${newPage}`).addClass('current');
+  $(`.nav-${oldPage}`).removeClass('current');
 
   // Changing page
-  if (newPage === 'nav-about-me') {
-    $('#about-me-page').show(500);
-    $('#portfolio-page').hide(500);
-  } else {
-    $('#about-me-page').hide(500);
-    $('#portfolio-page').show(500);
-  }
+    $(`#${newPage}-page`).show(500);
+    $(`#${oldPage}-page`).hide(500);
 };
 
 $(() => {
   // Swapping pages
   $('.nav-about-me').click(() => {
-    pageSwap('nav-about-me', 'nav-portfolio');
+    pageSwap('about-me', 'portfolio');
   });
 
   $('.nav-portfolio').click(() => {
-    pageSwap('nav-portfolio', 'nav-about-me');
+    pageSwap('portfolio', 'about-me');
   });
 
   // Activating tooltip
