109bd995c870b24e6a6ae66b8bc67d0f86bb8c83
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-30 02:10:00 +0100

    Added pageswap function

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+$(() => {
+  const pageSwap = (newPage, oldPage) => {
+    // Changing nav
+    $(`.${newPage}`).attr('class', `${newPage} nav-link current`);
+    $(`.${oldPage}`).attr('class', `${oldPage} nav-link`);
+
+    // Changing page
+    if (newPage === 'nav-about-me') {
+      $('#about-me-page').show('fast');
+      $('#portfolio-page').hide('fast');
+      
+    } else {
+      $('#about-me-page').hide('fast');
+      $('#portfolio-page').show('fast');
+    }
+  };
+
+  $('.nav-about-me').click(() => {
+    pageSwap('nav-about-me', 'nav-portfolio');
+  });
+
+  $('.nav-portfolio').click(() => {
+    pageSwap('nav-portfolio', 'nav-about-me');
+  });
+});
