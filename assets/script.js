1e82c4f351d6eca1e8f5c1e5a260da16aa48f6f0
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-30 02:19:43 +0100

    Moved tooltip script

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,20 @@
+const pageSwap = (newPage, oldPage) => {
+  // Changing nav
+  $(`.${newPage}`).attr('class', `${newPage} nav-link current`);
+  $(`.${oldPage}`).attr('class', `${oldPage} nav-link`);
+
+  // Changing page
+  if (newPage === 'nav-about-me') {
+    $('#about-me-page').show('fast');
+    $('#portfolio-page').hide('fast');
+  } else {
+    $('#about-me-page').hide('fast');
+    $('#portfolio-page').show('fast');
+  }
+};
+
 $(() => {
-  const pageSwap = (newPage, oldPage) => {
-    // Changing nav
-    $(`.${newPage}`).attr('class', `${newPage} nav-link current`);
-    $(`.${oldPage}`).attr('class', `${oldPage} nav-link`);
-
-    // Changing page
-    if (newPage === 'nav-about-me') {
-      $('#about-me-page').show('fast');
-      $('#portfolio-page').hide('fast');
-      
-    } else {
-      $('#about-me-page').hide('fast');
-      $('#portfolio-page').show('fast');
-    }
-  };
-
+  // Swapping pages
   $('.nav-about-me').click(() => {
     pageSwap('nav-about-me', 'nav-portfolio');
   });
@@ -22,4 +22,7 @@ $(() => {
   $('.nav-portfolio').click(() => {
     pageSwap('nav-portfolio', 'nav-about-me');
   });
+
+  // Activating tooltip
+  $('a').tooltip();
 });
